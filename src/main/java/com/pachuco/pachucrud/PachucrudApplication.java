0c95b045aa5d05c5cca5f0bb4fe42d3b032b2bfb
unknown <jccnnetwork@gmail.com>
2026-05-06 18:58:15 -0600

    java update

---
+++ src/main/java/com/pachuco/pachucrud/PachucrudApplication.java
@@ -0,0 +1,13 @@
+package com.pachuco.pachucrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PachucrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PachucrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pachuco/pachucrud/PachucrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pachuco.pachucrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PachucrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
